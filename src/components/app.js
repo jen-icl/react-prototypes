bc62ca6466948661b45ebf69323c8682bc7257ab
I Chun Lai <jenniferlai.81@gmail.com>
2019-03-26 18:19:39 -0700

    Functional Components - I Chun Lai

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,20 @@
+import React from 'react';
+import Table from './table';
 
+const students = [
+    {name: 'Bruce Lee', course: 'Say water', grade: 90},
+    {name: 'Jacky Chan', course: 'Get injured', grade: 85},
+    {name: 'Ip Man', course: 'Yong Chun', grade: 99},
+];
 
+const App = () => {
+
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    );
+}
+
+export default App;
