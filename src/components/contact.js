9953846afbfc1186487df41f01ba34fb845a4d83
I Chun Lai <jenniferlai.81@gmail.com>
2019-04-01 18:54:19 -0700

    Routing intro - I Chun Lai

---
+++ src/components/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => {
+    return(
+        <div>
+            <h1>Contact Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sequi ducimus unde delectus hic illo.</p>
+        </div>
+    );
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div>
+            <h1>Welcome to this Page!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia similique cumque nihil reiciendis, vero ex accusamus alias maiores libero non, et asperiores dolores quod natus.</p>
+        </div>
+    );
+}
